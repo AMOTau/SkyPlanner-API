6ac82b7482ae2d01c92d35fa36c0b37114eb6c40
AMOTau <ogonatau@gmail.com>
2025-01-14 10:29:07 +0200

    hashed the password

--- models/User.js
+++ models/User.js
@@ -14,12 +14,21 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    
+
     },
     favorites: [
         {
-          locationName: { type: String, required: true },
-          id: { type: String, required: true },
+            locationName: { type: String, required: true },
+            id: { type: String, required: true },
         },
-      ],
+    ],
 });
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
